9835a33218d95a9165bfd9957d53786de51bfe5d
Nevo Cohen <nevocohen111@gmail.com>
2022-05-27 13:37:18 +0300

    git commit -m "mongoDB"

--- src/main/java/mongoDB/App.java
+++ src/main/java/mongoDB/App.java
@@ -34,7 +34,6 @@ public class App {
 
     }
 
-
     public static void addPatient() {
         boolean isHealthy = false;
         String name = validateInput("Enter patient's name :",String.class);
